cb9ea9420ecd4f4f35429e77fb96e346c3c2a816
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-01-11 12:51:03 +0100

    slider fungerer ikke

--- forside.php
+++ forside.php
@@ -3,6 +3,7 @@
 <?php get_header(); ?>
 
 <!-- Hero image, text and call to action -->
+
 <div class="hero">
 
 	<?php $hero_image = get_field( 'hero_image' ); ?>
@@ -11,32 +12,22 @@
 	<?php } ?>
 
 	<div class="hero_info">
+
 		<h1 class="hero_title"><?php the_field('hero_title'); ?></h1>
 		<p><?php the_field('hero_tekst'); ?></p>
+
 		<button class="calltoaction" type="button" name="button">
 			<a class="hero_button" href="<?php the_field('button_url'); ?>"><?php the_field('button_text'); ?> &#8594;</a>
 		</button>
 	</div>
 
+
+
 </div>
 
 <div class="case">
-	<?php
 
-$images = get_field('case_slider');
 
-if( $images ): ?>
-    <ul>
-        <?php foreach( $images as $image ): ?>
-            <li>
-                <a href="<?php echo $image['url']; ?>">
-                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
-                </a>
-                <p><?php echo $image['caption']; ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
 
 </div>
 
