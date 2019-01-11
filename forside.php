d02a51dbcfdef517b262307088cb23c4aad3c406
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-01-11 11:01:22 +0100

    rettelser

--- forside.php
+++ forside.php
@@ -14,13 +14,30 @@
 		<h1><?php the_field('hero_title'); ?></h1>
 		<p><?php the_field('hero_tekst'); ?></p>
 		<button class="calltoaction" type="button" name="button">
-			<a class="hero_button" href="<?php the_field('button_url'); ?>"><?php the_field('button_text'); ?></a>
+			<a class="hero_button" href="<?php the_field('button_url'); ?>"><?php the_field('button_text'); ?> &#8594;</a>
+
 		</button>
 	</div>
 
 </div>
 
 <div class="case">
+	<?php
+
+$images = get_field('case_slider');
+
+if( $images ): ?>
+    <ul>
+        <?php foreach( $images as $image ): ?>
+            <li>
+                <a href="<?php echo $image['url']; ?>">
+                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                </a>
+                <p><?php echo $image['caption']; ?></p>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
 
 </div>
 
