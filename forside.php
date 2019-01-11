fb322d89617310d3b962445212048c0ce492e3c3
Surkrit <jensendennis520@gmail.com>
2019-01-11 11:02:04 +0100

    pil tilføjet til hero button

--- forside.php
+++ forside.php
@@ -15,6 +15,10 @@
 		<p><?php the_field('hero_tekst'); ?></p>
 		<button class="calltoaction" type="button" name="button">
 			<a class="hero_button" href="<?php the_field('button_url'); ?>"><?php the_field('button_text'); ?></a>
+			<svg id="arrowright" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="10px" height="30px" viewBox="0 0 50 80" xml:space="preserve">
+    <polyline fill="none" stroke="#FFFFFF" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" points="
+		0.375,0.375 45.63,38.087 0.375,75.8 "/>
+  	</svg>
 		</button>
 	</div>
 
