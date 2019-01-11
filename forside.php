0f4606fc4d8fea0a72b024b7b54b634ad13afccc
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-01-11 13:19:35 +0100

    flexslider

--- forside.php
+++ forside.php
@@ -27,7 +27,31 @@
 
 <div class="case">
 
+	<?php
 
+	$images = get_field('case_slider');
+
+	if( $images ): ?>
+	    <div id="slider" class="flexslider">
+	        <ul class="slides">
+	            <?php foreach( $images as $image ): ?>
+	                <li>
+	                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+	                    <p><?php echo $image['caption']; ?></p>
+	                </li>
+	            <?php endforeach; ?>
+	        </ul>
+	    </div>
+	    <div id="carousel" class="flexslider">
+	        <ul class="slides">
+	            <?php foreach( $images as $image ): ?>
+	                <li>
+	                    <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+	                </li>
+	            <?php endforeach; ?>
+	        </ul>
+	    </div>
+	<?php endif; ?>
 
 </div>
 
