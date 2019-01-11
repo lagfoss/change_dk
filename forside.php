cbefaa736e2e8eabdfda83d42dc1e9547b7e06cd
Surkrit <jensendennis520@gmail.com>
2019-01-11 13:20:25 +0100

    Merge branch 'master' of https://github.com/lagfoss/change_dk

--- forside.php
+++ forside.php
@@ -26,22 +26,8 @@
 </div>
 
 <div class="case">
-	<?php
 
-$images = get_field('case_slider');
 
-if( $images ): ?>
-    <ul>
-        <?php foreach( $images as $image ): ?>
-            <li>
-                <a href="<?php echo $image['url']; ?>">
-                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
-                </a>
-                <p><?php echo $image['caption']; ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
 
 </div>
 
