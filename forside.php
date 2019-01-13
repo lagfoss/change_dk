197b86b9e998de6264eb8ab026f3251156fe3f14
Surkrit <jensendennis520@gmail.com>
2019-01-13 20:14:27 +0100

    fixed nogen classes

--- forside.php
+++ forside.php
@@ -4,14 +4,14 @@
 
 <!-- Hero image, text and call to action -->
 
-<div class="container hero">
+<div class="hero">
 
 	<?php $hero_image = get_field( 'hero_image' ); ?>
 	<?php if ( $hero_image ) { ?>
 		<img src="<?php echo $hero_image['url']; ?>" alt="<?php echo $hero_image['alt']; ?>" />
 	<?php } ?>
 
-	<div class="hero_info">
+	<div class="container-fluid hero_info">
 
 		<h1 class="hero_title"><?php the_field('hero_title'); ?></h1>
 		<p><?php the_field('hero_tekst'); ?></p>
@@ -20,12 +20,12 @@
 			<a class="hero_button" href="<?php the_field('button_url'); ?>"><?php the_field('button_text'); ?> &#8594;</a>
 		</button>
 
-<div class="test">
-	<img src="img/play-circle-copy.svg"
+		<div class="container-fluid test">
+		<img src="img/play-circle-copy.svg"
      class="play-circle-Copy">
 		<p class="Watch">Watch our:</p>
 		<p class="Showreel">Showreel</p>
-</div>
+		</div>
 	</div>
 
 
