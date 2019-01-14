6097ec8fb9aa14b9bbbbf8c6cbed0f2b1180f205
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-01-14 14:54:47 +0100

    single-case.php

--- functions.php
+++ functions.php
@@ -71,4 +71,60 @@ add_action('wp_print_styles', 'startwordpress_google_fonts');
 
 
 
+// Register a custom post type called “case”.
+// @see get_post_type_labels() for label keys.
+
+function my_cases() {
+   $labels = array(
+       'name'                  => _x( 'Produkter', 'Post type general name', 'textdomain' ),
+       'singular_name'         => _x( 'Produkt', 'Post type singular name', 'textdomain' ),
+       'menu_name'             => _x( 'Produkter', 'Admin Menu text', 'textdomain' ),
+       'name_admin_bar'        => _x( 'Produkt', 'Add New on Toolbar', 'textdomain' ),
+       'add_new'               => __( 'Add New', 'textdomain' ),
+       'add_new_item'          => __( 'Add New Produkt', 'textdomain' ),
+       'new_item'              => __( 'New Produkt', 'textdomain' ),
+       'edit_item'             => __( 'Edit Produkt', 'textdomain' ),
+       'view_item'             => __( 'View Produkt', 'textdomain' ),
+       'all_items'             => __( 'All Produkter', 'textdomain' ),
+       'search_items'          => __( 'Search Produkter', 'textdomain' ),
+       'parent_item_colon'     => __( 'Parent Produkter:', 'textdomain' ),
+       'not_found'             => __( 'No Produkter found.', 'textdomain' ),
+       'not_found_in_trash'    => __( 'No Produkter found in Trash.', 'textdomain' ),
+       'featured_image'        => _x( 'Produkter Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+       'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+       'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+       'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+       'archives'              => _x( 'Produkter archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+       'insert_into_item'      => _x( 'Insert into Produkt', 'Overrides the “Insert into post”/“Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+       'uploaded_to_this_item' => _x( 'Uploaded to this Produkt', 'Overrides the “Uploaded to this post”/“Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+       'filter_items_list'     => _x( 'Filter cases list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/“Filter pages list”. Added in 4.4', 'textdomain' ),
+       'items_list_navigation' => _x( 'Produkter list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/“Pages list navigation”. Added in 4.4', 'textdomain' ),
+       'items_list'            => _x( 'Produkter list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/“Pages list”. Added in 4.4', 'textdomain' ),
+   );
+
+   $args = array(
+      'labels'             => $labels,
+      'public'             => true,
+      'publicly_queryable' => true,
+      'show_ui'            => true,
+      'show_in_menu'       => true,
+      'query_var'          => true,
+      'rewrite'            => array( 'slug' => 'cases' ),
+      'capability_type'    => 'page',
+      'has_archive'        => true,
+      'hierarchical'       => false,
+      'orderby'            => 'menu_order',
+      'order'              => 'ASC',
+      'menu_position'      => null,
+      'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail','page-attributes' ),
+                'taxonomies' => array( 'post_tag', 'category' ),
+   );
+
+register_post_type( 'cases', $args );
+
+}
+
+add_action( 'init', 'my_cases' );
+
+
 ?>
---
+++ single-case.php
@@ -0,0 +1,69 @@
+<?php if( have_rows('case_slider') ): ?>
+
+  <ul class="slides">
+
+    <?php while( have_rows('case_slider') ): the_row();
+
+    // vars
+    $title = get_sub_field('case_title');
+    $category = get_sub_field('case_category');
+    $text = get_sub_field('case_text');
+    $button = get_sub_field('case_button_text');
+    $link = get_sub_field('case_button_url');
+    $image = get_sub_field('case_image');
+
+    ?>
+
+    <li class="slide">
+      <div class="row">
+        <div class="col-7 order-2 case_img style="background-image:url(<?php the_field('hero_image'); ?>);"">
+
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+
+      </div>
+
+      <div class="col-3 order-1 case_info">
+        <p>CASE:</p>
+      <h2><?php echo $title; ?></h2>
+      <p class="cat"><?php echo $category; ?></p>
+      <p><?php echo $text; ?><p>
+        <button type="button" name="button"><a class="hero_button" href="<?php echo $link; ?>"><?php echo $button; ?> &#8594;</a></button>
+    </div>
+    </div>
+
+    </li>
+
+  <?php endwhile; ?>
+
+</ul>
+
+<?php endif; ?>
+
+<?php
+   $params = array('posts_per_page' => 10,'orderby' => 'menu_order',
+   'order' => 'ASC', 'post_type' => 'products');
+   $query = new WP_Query($params);
+   ?>
+    <?php if ($query->have_posts()) : ?>
+    <?php $counter = 01;?>
+    <?php while ($query->have_posts()) :
+      $query->the_post(); ?>
+    <div class="product_content">
+      <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+      <a href="<?php the_permalink(); ?>" class="product_slider_img" style="background: url('<?php echo $backgroundImg[0]; ?>') no-repeat;"></a>
+      <?php $counter++; // add one per row ?>
+      <a class="product_link" href="<?php the_permalink(); ?>">
+        <?php the_title();?></a>
+      <p><?php the_excerpt(); ?></p>
+      <a class="btn" href="<?php the_permalink(); ?>"><?php the_field('products_btn_text');?></a>
+    </div>
+
+  <?php endwhile; ?>
+
+  <?php wp_reset_postdata(); ?>
+
+  <?php else:  ?>
+
+  <li>
+  <?php _e( 'No cases' ); ?>
+  <?php endif; ?>
