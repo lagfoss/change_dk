1ef66eb319597f572a7f017deda88f820e009273
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-01-14 09:44:11 +0100

    hero fix

--- forside.php
+++ forside.php
@@ -11,7 +11,6 @@
 		<img src="<?php echo $hero_image['url']; ?>" alt="<?php echo $hero_image['alt']; ?>" />
 	<?php } ?>
 
-	<div class="col hero_info">
 	<div class="hero_info">
 
 		<h1 class="hero_title"><?php the_field('hero_title'); ?></h1>
@@ -20,12 +19,8 @@
 		<button class="calltoaction" type="button" name="button">
 			<a class="hero_button" href="<?php the_field('hero_button_url'); ?>"><?php the_field('hero_button_text'); ?> &#8594;</a>
 		</button>
-		<div class="row test">
-			<img src="images/play-circle-copy.svg"
-				 class="play-circle-Copy">
-				<p class="Watch">Watch our:</p>
-				<p class="Showreel">Showreel</p>
-		</div>
+	</div>
+
 
 <div class="watchourshowreel">
 	<img src="images/play-circle-copy.svg"
@@ -33,10 +28,7 @@
 		<p class="Watch">Watch our:</p>
 		<p class="Showreel">Showreel</p>
 </div>
-	</div>
 
-
-</div>
 </div>
 
 <div class="case_area">
