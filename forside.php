f080aba53a90c11b4f5b7a2491894b1dbd8d4ea7
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-01-17 14:21:27 +0100

    process img background

--- forside.php
+++ forside.php
@@ -45,7 +45,7 @@
 
 		    <li class="slide">
 		      <div class="row">
-		        <div class="col-7 order-2 case_img style="background-image:url(<?php the_field('hero_image'); ?>);"">
+		        <div class="col-7 order-2 case_img">
 
 		        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
 
@@ -72,12 +72,9 @@
 	</div>
 </div>
 
-<section>
-
 <div class="container-fluid">
-<div class="row ourprocess">
-	<div class="col-6">
-		<?php the_field('process_image'); ?>
+<div class="row">
+	<div class="col-6 ourprocess" style="background-image:url(<?php the_field('process_image'); ?>);">
 	</div>
 
 	<div class="col-6 process">
@@ -109,43 +106,40 @@
 </div>
 </div>
 
-</section>
 
 <div class="what-can-we-do">
-	<div class="row test">
-			<div class="col-sm hover1">
+
+	<div class="row">
+			<div class="col-sm">
 						<div class="col-sm-12">
 							<h2 class="wcwd_header"><?php the_field('wcwd_header'); ?></h2>
 							<p class="wcwd_text"><?php the_field('wcwd_text'); ?></p>
 						</div>
 				</div>
+			<div class="col-sm hover">
 
-			<div class="col-sm hover">
+					</a>
+					<div class="wcwd-content col-sm-12">
+						<?php if( get_field('brandstrategi_img') ): ?>
+						<img src="<?php the_field('http://xn--kronbr-tua.dk/wp-content/uploads/2019/01/linkedin.svg'); ?>" />
+						<?php endif; ?>
+						<h6 class="wcwd_headline"><?php the_field('brandstrategi_header'); ?></h6>
+						<p class="wcwd_text"><?php the_field('brandstrategi_text'); ?></p>
+					</div>
+			</div>
+		<div class="col-sm hover">
+
+				</a>
 				<div class="wcwd-content col-sm-12">
-					<?php if( get_field('brandstrategi_img') ): ?>
-					<img src="<?php the_field('http://xn--kronbr-tua.dk/wp-content/uploads/2019/01/linkedin.svg'); ?>" />
-					<?php endif; ?>
-					<h6 class="wcwd_headline"><?php the_field('brandstrategi_header'); ?></h6>
-					<p class="wcwd_text"><?php the_field('brandstrategi_text'); ?></p>
-				</div>
-		</div>
-			<div class="col-sm hover">
-				<div class="wcwd-content col-sm-12">
-					<?php if( get_field('kommunikationsdesign_img') ): ?>
-					<img src="<?php the_field('http://xn--kronbr-tua.dk/wp-content/uploads/2019/01/linkedin.svg'); ?>" />
-					<?php endif; ?>
 					<h6 class="wcwd_headline"><?php the_field('kommunikationsdesign_header'); ?></h6>
 					<p class="wcwd_text"><?php the_field('kommunikationsdesign_text'); ?></p>
 				</div>
 		</div>
-	</div>
 
-<div class="row test">
 		<div class="col-sm hover">
+
+				</a>
 				<div class="wcwd-content col-sm-12">
-					<?php if( get_field('social_media_img') ): ?>
-					<img src="<?php the_field('http://xn--kronbr-tua.dk/wp-content/uploads/2019/01/linkedin.svg'); ?>" />
-					<?php endif; ?>
 					<h6 class="wcwd_headline"><?php the_field('social_media_header'); ?></h6>
 					<p class="wcwd_text"><?php the_field('social_media_text'); ?></p>
 				</div>
@@ -155,9 +149,6 @@
 
 				</a>
 				<div class="wcwd-content col-sm-12">
-					<?php if( get_field('pressemeddelelse_img') ): ?>
-					<img src="<?php the_field('http://xn--kronbr-tua.dk/wp-content/uploads/2019/01/linkedin.svg'); ?>" />
-					<?php endif; ?>
 					<h6 class="wcwd_headline"><?php the_field('pressemeddelelse_header'); ?></h6>
 					<p class="wcwd_text"><?php the_field('pressemeddelelse_text'); ?></p>
 				</div>
@@ -167,18 +158,15 @@
 
 				</a>
 				<div class="wcwd-content col-sm-12">
-					<?php if( get_field('kommunikationsstrategi_img') ): ?>
-					<img src="<?php the_field('http://xn--kronbr-tua.dk/wp-content/uploads/2019/01/linkedin.svg'); ?>" />
-					<?php endif; ?>
 					<h6 class="wcwd_headline"><?php the_field('kommunikationsstrategi_header'); ?></h6>
 					<p class="wcwd_text"><?php the_field('kommunikationsstrategi_text'); ?></p>
 				</div>
 		</div>
-	</div>
+
+		<button class="calltoaction" type="button" name="button">
+			<a class="hero_button" href="<?php the_field('hero_button_url'); ?>"><?php the_field('hero_button_text'); ?> &#8594;</a>
+		</button>
 </div>
-<button class="calltoaction" type="button" name="button">
-	<a class="hero_button" href="<?php the_field('hero_button_url'); ?>"><?php the_field('hero_button_text'); ?> &#8594;</a>
-</button>
 </div>
 
 <?php get_footer(); ?>
