e99883c970605fa15dae2e25ef892d9906bf956e
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-02-04 10:14:47 +0100

    langt fra færdig med modal

--- forside.php
+++ forside.php
@@ -32,7 +32,7 @@
 					$content2 = get_sub_field('showreel_showreel');
 					?>
 					<div class="watchourshowreel">
-						<img class="playcircle" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>">
+						<a href="#myModal" data-toggle="modal" data-src="https://www.youtube.com/embed/Jfrjeg26Cwk"><img class="playcircle" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>"></a>
 						<p class="Watch"><?php echo $content; ?></p>
 						<p class="Showreel"><?php echo $content2; ?></p>
 					</div>
@@ -236,5 +236,31 @@ $(window).scroll(function() {
 	$(".logo").html("<img src='<?php echo get_bloginfo('template_directory'); ?>/images/logo_neg.svg'>");
 }
 
+});
+
+
+$(document).ready(function() {
+
+// Gets the video src from the data-src on each button
+
+var $videoSrc;
+$('.video-btn').click(function() {
+    $videoSrc = $(this).data( "src" );
+});
+console.log($videoSrc);
+
+// when the modal is opened autoplay it
+$('#myModal').on('shown.bs.modal', function (e) {
+
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+$("#video").attr('src',$videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" );
+})
+
+// stop playing the youtube video when I close the modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src',$videoSrc);
+})
+// document ready
 });
 </script>
