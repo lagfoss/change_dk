78bbe9e87a3bfe4996b5d767d3da7b3f1dd37727
Surkrit <jensendennis520@gmail.com>
2019-02-05 12:39:11 +0100

    Rani fik sku en rimelig okay idé *golfklap*

--- header.php
+++ header.php
@@ -70,35 +70,15 @@
         <span class="example header"> <?php get_search_form(); ?> </span>
 
         <script type="text/javascript">
-        function toggleClass(el, className) {
-        var el = document.querySelectorAll(el);
-
-        for (i = 0; i < el.length; i++) {
-
-          if (el[i].classList) {
-            el[i].classList.toggle(className);
-          } else {
-              var classes = el[i].className.split(' ');
-              var existingIndex = -1;
-              for (var j = classes.length; j--;) {
-                if (classes[j] === className)
-                  existingIndex = j;
-              }
-
-              if (existingIndex >= 0)
-                classes.splice(existingIndex, 1);
-              else
-                classes.push(className);
-
-            el[i].className = classes.join(' ');
-          }
-        }
-        }
+        $(".menubtn").click(function(){
+            $(".header").stop();
+        });
+        );
 
         </script>
 
 
-        <div onclick="toggleClass('.example', 'header')" id="cf_onclick" class="menubtn">
+        <div onclick="toggleClass('.example', '.header')" id="cf_onclick" class="menubtn">
           <span></span>
         </div>
 
