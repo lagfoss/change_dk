db76cb6b7655bff74f3aa1af0dd707cd08a0587c
Surkrit <jensendennis520@gmail.com>
2019-02-05 14:41:48 +0100

    flere ydelser

--- ydelser.php
+++ ydelser.php
@@ -7,16 +7,25 @@
     <div class="row background_img_ydelser" style="background-image:url(<?php the_field('background_img_ydelser'); ?>);">
   		<div class="col-md-6">
         <div class="hero_info">
-          <div class="stroke_position">
-            <div class="stroke"></div>
+          <div class="stroke_position_ydelser">
+            <div class="stroke_ydelser"></div>
           </div>
           <h1 class="h1_hero_ydelser"><?php the_field('h1_hero_ydelser'); ?></h1>
-
-          <a class="calltoaction_hero_ydelser" href="#"> <?php if( get_field('calltoaction_hero_ydelser') ): ?>
-            <img class="1" src="<?php the_field('calltoaction_hero_ydelser'); ?>" />
-            <?php endif; ?></a>
         </div>
   		</div>
+      <div class="col-md-6">
+        <a class="" href="#"> <?php if( get_field('calltoaction_hero_ydelser') ): ?>
+          <img class="calltoaction_hero_ydelser" src="<?php the_field('calltoaction_hero_ydelser'); ?>" />
+          <?php endif; ?></a>
+      </div>
+  	</div>
+  </div>
+
+
+  <div class="container-fluid">
+    <div class="row">
+      <h2 class="ydelser_main_content_overskrift"> <?php the_field('ydelser_main_content_overskrift') ?> </h2>
+      <p class="ydelser_main_content_text"> <?php the_field('ydelser_main_content_text') ?> </p>
   	</div>
   </div>
 
