30009d4728a324fff87435cd50b70eeda56db1f3
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-02-06 15:36:00 +0100

    så fungerer scroll på header

--- header.php
+++ header.php
@@ -146,7 +146,7 @@
 
           // normalize treshold range
           treshold = (treshold+diff>headerHeight) ? headerHeight : treshold+diff;
-          treshold = (treshold < 0) ? 0 : treshold;
+          treshold = (treshold < 0) ? -20 : treshold;
 
           header.css('top', (-treshold)+'px');
 
