bcd6f7855f99f62f7bc31c51837e8cc0cd2d6e84
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-02-15 15:33:08 +0100

    nu må det gerne blive weekend

--- single-case.php
+++ single-case.php
@@ -239,7 +239,7 @@ Template Post Type: cases
             <div class="container-fluid">
               <div class="row">
                 <div class="col-md-5 d-flex justify-content-end align-items-center">
-                  <div class="rekru_content">
+                  <div class="brand_content">
                     <div class="row stroke_position_case">
                       <div class="stroke_case"></div>
                       <p class="pre_client"><?php echo $prebrand; ?></p>
