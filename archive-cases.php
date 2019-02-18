e30d35b54e6f74790d55284c322ea1214c2b381a
Surkrit <jensendennis520@gmail.com>
2019-02-18 12:50:18 +0100

    paninate implementeret

--- archive-cases.php
+++ archive-cases.php
@@ -17,8 +17,8 @@
       <!-- Image carousel -->
 
     <?php
-    $params = array('posts_per_page' => 5,'orderby' => 'menu_order',
-    'order' => 'ASC', 'post_type' => 'cases');
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $params = array('posts_per_page' => 1, 'paged' => $paged,'orderby' => 'menu_order', 'order' => 'ASC', 'post_type' => 'cases',);
     $query = new WP_Query($params);
     ?>
           <?php if ($query->have_posts()) : ?>
@@ -54,12 +54,34 @@
                 </div></a>
 
                 <?php endwhile; ?>
-                  <?php wp_reset_postdata(); ?>
+                <div class="pagination">
+                <?php
+                    echo paginate_links( array(
+                        'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+                        'total'        => $query->max_num_pages,
+                        'current'      => max( 1, get_query_var( 'paged' ) ),
+                        'format'       => '?paged=%#%',
+                        'show_all'     => true,
+                        'type'         => 'plain',
+                        'end_size'     => 2,
+                        'mid_size'     => 1,
+                        'prev_next'    => true,
+                        'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
+                        'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'text-domain' ) ),
+                        'add_args'     => false,
+                        'add_fragment' => '',
+                    ) );
+                ?>
+              </div>
+
                     <?php else:  ?>
 
                         <?php _e( 'No cases' ); ?>
 
                           <?php endif; ?>
+
+                          <?php ?>
+
 
 
 <?php get_footer();?>
