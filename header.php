893bff76a1035d015c38602d2813cba5075a3cad
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-02-19 09:33:20 +0100

    search skjult

--- header.php
+++ header.php
@@ -67,7 +67,7 @@
         <img class="logoimg" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo_neg.svg" alt=""></a>
 
       <div class="container-fluid">
-        <?php get_search_form(); ?>
+        <!-- <?php get_search_form(); ?> -->
 
         <div id="cf_onclick" class="menubtn">
           <span></span>
