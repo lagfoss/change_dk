09e546b8993364c9119f6482f3c4bd08411fe3fa
Surkrit <jensendennis520@gmail.com>
2019-02-20 11:13:12 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -151,6 +151,25 @@ ScrollReveal().reveal('.contact_footer', { distance: '250px' });
 
 
 
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    // Enable thumbnails
+    thumbs: true,
+
+    // When only using images in your slide (like the demo) use this option to dynamicly create thumbnails without using the attribute data-thumb.
+    thumbImage: false,
+
+    // Enable this if you have pre-rendered thumbnails in your html instead of letting this plugin generate them. This is recommended as it will prevent FOUC
+    thumbsPrerendered: true,
+
+    // Class that will be used on the thumbnail container
+    thumbContainerClass: 'owl-thumbs',
+
+    // Class that will be used on the thumbnail item's
+    thumbItemClass: 'owl-thumb-item'
+  });
+});
+
 
 
 </script>
