eacb59d3ece0fd9a9db15a7523f7bb69a504301a
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-03-06 09:55:10 +0100

    Footer til kontakt siden

--- kontakt.php
+++ kontakt.php
@@ -2,11 +2,11 @@
 
 <?php get_header(); ?>
 <body <?php body_class(whitebg); ?>>
-  
+
   <div class="container-fluid">
     <div class="row">
 
 	  </div>
   </div>
 
-<?php get_footer(); ?>
+<?php get_footer(kontakt); ?>
