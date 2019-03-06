da08d6b342a4673cc1e584fb50d07396991e64e8
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-03-06 11:18:59 +0100

    nu kan man altid skirve til Dennis :laughing:

--- kontakt.php
+++ kontakt.php
@@ -4,9 +4,21 @@
 <body <?php body_class(whitebg); ?>>
 
   <div class="container-fluid">
-    <div class="row">
-      <?php echo do_shortcode("[contact-form-7 id=1061 title=Contact form 1]") ; ?>
+    <div class="row wrapper">
+      <div class="col-6-md contact_intro">
+        <h1 class="black"><?php the_field('contact_title'); ?></h1>
+        <p class="gray"><?php the_field('contact_text'); ?></p>
+      </div>
+      <div class="col-6-md contact_img">
+
+      </div>
 	  </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row wrapper">
+        <?php echo do_shortcode("[contact-form-7 id=1061 title=Contact form 1]") ; ?>
+    </div>
+
   </div>
 
 <?php get_footer(kontakt); ?>
