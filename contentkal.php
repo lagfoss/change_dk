0779aa6e0cbbada65357d1e77562e54e7d2df4de
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-03-21 11:42:33 +0100

    Update contentkal.php

--- contentkal.php
+++ contentkal.php
@@ -133,20 +133,4 @@ $(".menubtn").addClass("menu_black");
     }
 });
 
-ScrollReveal().reveal('.stroke_position_services', { distance: '450px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('h1', { distance: '500px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.calltoaction_hero_services', { distance: '550px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.services_main_content_overskrift', { distance: '450px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.services_main_content_text', { distance: '650px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.stroke', { distance: '550px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.wcwd_img', { distance: '300px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.scroll_header', { distance: '500px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.scroll_text', { distance: '600px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.services_dots', { distance: '450px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.stroke_services', { distance: '450px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('h2', { distance: '450px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.services_button', { distance: '550px', easing: 'ease-in-out', duration: 1000 });
-ScrollReveal().reveal('.1', { distance: '650px', easing: 'ease-in-out', duration: 700 });
-ScrollReveal().reveal('.logo_slider', { distance: '450px', easing: 'ease-in-out', duration: 700 });
-
 </script>
