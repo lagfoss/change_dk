83da1b2e32c353e1b50c45d37e7e519a15b32a35
lagfoss <32448248+lagfoss@users.noreply.github.com>
2019-03-27 09:01:07 +0100

    scrollReveal på single case done

--- single-case.php
+++ single-case.php
@@ -400,15 +400,16 @@ $(".header").addClass("out_white");
 
 ScrollReveal().reveal('.stroke_position_case', { distance: '250px', easing: 'ease-in-out', duration: 700});
 ScrollReveal().reveal('.case_hero h1', { distance: '250px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.client_cat', { distance: '250px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.client_about', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.case_client', { distance: '250px', easing: 'ease-in-out', duration: 700});
 ScrollReveal().reveal('.client_image', { distance: '250px', easing: 'ease-in-out', duration: 700});
 ScrollReveal().reveal('.goal_line', { distance: '250px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.case_some_ex', { distance: '250px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.case_link', { distance: '250px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.h2_case', { distance: '350px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.client_logo', { distance: '450px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.post-categories', { distance: '450px', easing: 'ease-in-out', duration: 700});
-ScrollReveal().reveal('.case_strategi_img', { distance: '450px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.case_strategi', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.strategi_dots', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.case_strategi_img', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.case_some_content', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.case_some_half', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.rekru_wrapper', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.brand', { distance: '250px', easing: 'ease-in-out', duration: 700});
+ScrollReveal().reveal('.case_results', { distance: '250px', easing: 'ease-in-out', duration: 700});
 
 </script>
